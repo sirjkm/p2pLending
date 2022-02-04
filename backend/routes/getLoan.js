a0c188b998652bdc2b94733dd268defb34bda878
Jason Manning <jasonkylemanning@gmail.com>
2022-02-03 19:25:49 -0500

    add crud operations

--- backend/routes/getLoan.js
+++ backend/routes/getLoan.js
@@ -25,4 +25,31 @@ router.route('/add').post((req, res) => {
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/:id').get((req, res) => {
+    getLoan.findById(req.params.id)
+        .then(request => res.json(request))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+    getLoan.findByIdAndDelete(req.params.id)
+        .then(() => res.json('Request deleted.'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req, res) => {
+    getLoan.findById(req.params.id)
+        .then(request => {
+            request.username = req.body.username;
+            request.amount = Number(req.body.amount);
+            request.duration = Number(req.body.duration);
+            request.date = Date.parse(req.body.date);
+
+            request.save()
+                .then(() => res.json('Request updated!'))
+                .catch(err => res.status(400).json('Error: ' + err));
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
 module.exports = router;
--- backend/routes/giveLoan.js
+++ backend/routes/giveLoan.js
@@ -1,1 +1,28 @@
-const router = require("express").Router();
+const router = require('express').Router();
+let giveLoan = require('../models/giveLoan.model');
+
+router.route('/').get((req, res) => {
+    giveLoan.find()
+        .then(offers => res.json(offers))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post((req, res) => {
+    const username = req.body.username;
+    const amount = Number(req.body.amount);
+    const duration = Number(req.body.duration);
+    const date = Date.parse(req.body.date);
+
+    const newRequest = new giveLoan({
+        username,
+        amount,
+        duration,
+        date,
+    });
+
+    newRequest.save()
+        .then(() => res.json('Offer added!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+module.exports = router;
