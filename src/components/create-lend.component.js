618d4dd735c29e97d2db0c468ce694f6eacf324c
Jason Manning <jasonkylemanning@gmail.com>
2022-02-28 22:13:31 -0500

    update create-lend component

--- src/components/create-lend.component.js
+++ src/components/create-lend.component.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React from "react";
+import axios from "axios";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 export default class CreateLend extends React.Component {
     render() {
