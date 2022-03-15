9b1d6793e4d43b846d7cccf7d2be815fdd5333ad
Jason Manning <jasonkylemanning@gmail.com>
2022-03-14 22:33:35 -0400

    update

--- src/components/borrow-lend-list.component.js
+++ src/components/borrow-lend-list.component.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 const Borrow = props => (
     <tr>
         <td>{props.borrow.username}</td>
-        <td>{props.borrow.username}</td>
+        <td>{props.borrow.lender}</td>
         <td>{props.borrow.amount}</td>
         <td>{props.borrow.duration}</td>
         <td>{props.borrow.date.substring(0, 10)}</td>
